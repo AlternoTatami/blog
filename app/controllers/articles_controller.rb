cf0b2a55303e816a363e93992db65701942c01ff
Victor Cedeno <vicedeno@gmail.com>
2024-03-11 22:56:03 -0300

    Updates view - controllers to show error messages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
-  def new    
+
+  def new
     @article = Article.new
   end
 
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
@@ -22,30 +22,30 @@ class ArticlesController < ApplicationController
 
     #Article esla Clase del modelo
     @article = Article.new(article_params)
-        
+
     if @article.save
       redirect_to @article
       #redireciona a una URL con nombre para este registro
       #en este caso a Show envianmdo el parametro del id de articulo
     else
-      
-      render "new"
+
+      render "new", status: :unprocessable_entity
       #El método render se utiliza para que el @artículo El objeto se devuelve a la plantilla "nueva" cuando se procesa
-      
+
     end
 
   end
 
   def update
     @article = Article.find(params[:id])
-  
+
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
-  
+
 
 
 
